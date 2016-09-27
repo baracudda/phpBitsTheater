fc3973ca85f04593131f60be5c8f7317ee9b2862
baracudda <ryan.fischbach@istresearch.com>
2016-09-27 11:04:23 -0400

    UserParameterException added INVALID_ARGUMENT_VALUE.

--- app/outtakes/UserParameterException.php
+++ app/outtakes/UserParameterException.php
@@ -10,9 +10,11 @@ use BitsTheater\BrokenLeg ;
 	{
 		const ERR_FILTER_TOO_BROAD = 300 ;
 		const ERR_USER_PARAM_TOO_LONG = 400;
+		const ERR_INVALID_ARGUMENT_VALUE = 422 ;
 
 		const MSG_FILTER_TOO_BROAD = 'userParam/errmsg_filter_too_broad' ;
 		const MSG_USER_PARAM_TOO_LONG = "userParam/errmsg_param_too_long";
+		const MSG_INVALID_ARGUMENT_VALUE = 'userParam/errmsg_invalid_arg_value';
         
 	}//end class
 
--- res/i18n/en/UserParam.php
+++ res/i18n/en/UserParam.php
@@ -7,5 +7,7 @@ class UserParam extends BaseResources
 {
 	public $errmsg_filter_too_broad =
 		'Filter value [%s] is too broad to affect the result set.' ;
-    public $errmsg_param_too_long = "parameter '%s' is too long";
+    public $errmsg_invalid_arg_value =
+    	'Value [%2$s] is invalid for parameter [%1$s].' ;
+    public $errmsg_param_too_long = 'Parameter [%s] is too long.' ;
 }
