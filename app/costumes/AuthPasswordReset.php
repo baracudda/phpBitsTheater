f0a43deac6931f1b414c73666f5700bcbc2fa4bb
baracudda <ryan.fischbach@istresearch.com>
2017-04-19 16:02:32 -0400

    create an override-able AuthPasswordReset costume that descends from our CursorCloset version.

---
+++ app/costumes/AuthPasswordReset.php
@@ -0,0 +1,17 @@
+<?php
+namespace BitsTheater\costumes ;
+use BitsTheater\costumes\CursorCloset\AuthPasswordReset as BaseCostume;
+{ // begin namespace
+
+/**
+ * Provides helper functions for dealing with password reset requests. This
+ * costume applies to the AuthBasic model.
+ */
+class AuthPasswordReset extends BaseCostume
+{
+	
+	//nothing to override, yet
+		
+} // end AuthPasswordReset class
+
+} // end namespace
