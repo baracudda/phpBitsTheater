596bcb22194c66ad619f6329a221fbf7e4a50515
baracudda <ryan.fischbach@istresearch.com>
2017-10-23 16:50:06 -0400

    added simple CLI script to return a generated UUID.

---
+++ app/cli/actionGenerateUUID.php
@@ -0,0 +1,15 @@
+#!/usr/bin/php
+<?php
+use com\blackmoonit\Strings;
+
+//additional options we wish to parse besides the default -u -p and --host
+//nothing additional to do
+
+//standard means of setting up the environment to access the website via CLI
+//  the processed CLI options returned by the process_cli_options() are returned as well.
+$theCliOptions = require_once('cli_bootstrap.php');
+
+print(Strings::createUUID());
+
+//end output with an EOL so CLI prompt will alway appear correctly on a fresh line
+print(PHP_EOL);
