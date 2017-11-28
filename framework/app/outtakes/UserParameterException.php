fcaea9a7ccd04403119f3f5a76512a5a7c9642e4
baracudda <ryan.fischbach@istresearch.com>
2017-11-28 12:50:13 -0500

    add ACT_* consts to UserParameterException like its BrokenLeg ancestor.

--- framework/app/outtakes/UserParameterException.php
+++ framework/app/outtakes/UserParameterException.php
@@ -16,6 +16,10 @@ use BitsTheater\BrokenLeg ;
 		const MSG_USER_PARAM_TOO_LONG = "userParam/errmsg_param_too_long";
 		const MSG_INVALID_ARGUMENT_VALUE = 'userParam/errmsg_invalid_arg_value';
         
+		// Condition constants if you like to use them rather than remember strings to use.
+		const ACT_FILTER_TOO_BROAD = 'FILTER_TOO_BROAD';
+		const ACT_USER_PARAM_TOO_LONG = 'USER_PARAM_TOO_LONG';
+		const ACT_INVALID_ARGUMENT_VALUE = 'INVALID_ARGUMENT_VALUE';
 	}//end class
 
 }//end namespace
