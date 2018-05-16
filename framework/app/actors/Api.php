d4760801f77af86ae10049a57beb8071b531beea
Ryan Fischbach <ryan.fischbach@istresearch.com>
2018-05-16 15:50:59 -0400

    bugfix: actor Api now tests agaist a list of method prefixes and only calls upon ones defined with a scope of public, by default.

--- framework/app/actors/Api.php
+++ framework/app/actors/Api.php
@@ -9,6 +9,24 @@ class Api extends BaseActor
 {
 	
 	/**
+	 * Since the purpose of API class is to "redirect" the URL to the
+	 * actual actor/method being referenced, construct and return the list
+	 * of possible methods being called.
+	 * @param string $aPossibleAction - the action requested.
+	 * @return string[] Returns the list of possible matches to query.
+	 */
+	protected function getPossibleMethodList($aPossibleAction)
+	{
+		return array(
+				Strings::getMethodName($aPossibleAction),
+				Strings::getMethodName('ajaj_' . $aPossibleAction),
+				Strings::getMethodName('api_' . $aPossibleAction),
+				Strings::getMethodName('ajax_' . $aPossibleAction),
+		);
+	}
+	
+
+	/**
 	 * API action is "special" in that it can load ANY other Actor as if it was a method.
 	 * @see \BitsTheater\Actor::performAct()
 	 */
@@ -16,7 +34,19 @@ class Api extends BaseActor
 		if (!method_exists($this, $aAction)) {
 			$theActorClass = $this->getDirector()->getActorClass($aAction);
 			if (class_exists($theActorClass)) {
-				$theAction = Strings::getMethodName('ajaj_'.array_shift($aQuery));
+				$theAction = null;
+				$thePossibleMethodList = $this->getPossibleMethodList(array_shift($aQuery));
+				foreach($thePossibleMethodList as $thePossibleMethod) try {
+					$theMethod = new \ReflectionMethod($theActorClass, $thePossibleMethod);
+					if ( $theMethod->isPublic() ) {
+						$theAction = $thePossibleMethod;
+						break;
+					}
+				}
+				catch (\ReflectionException $rx)
+				{ continue; }
+				if ( empty($theAction) )
+				{ throw new FourOhFourExit($this->getSiteUrl($aAction . '/' . $thePossibleAction)); }
 				$theActor = new $theActorClass($this->getDirector(), $theAction);
 				$this->viewToRender('results_as_json'); //in case of error, JSON is returned
 				$theActor->viewToRender('results_as_json');
