084b461e587a4e4aa532db3c9b48c161dbf6f368
Ryan Fischbach <ryan.fischbach@istresearch.com>
2018-11-08 10:52:31 -0500

    CLI actions protect against execution as non-CLI; ensure the correct cli_bootstrap is loaded (in case you have multiple different versions of BitsTheater websites installed and executing from some other website's folder {cough TIWK cough}); website upgrade protects against execution if no auth table (since we now have migrate-specific CLI action).

--- framework/app/cli/actionWebsiteUpgrade.php
+++ framework/app/cli/actionWebsiteUpgrade.php
@@ -1,6 +1,8 @@
 #!/usr/bin/php
 <?php
-use BitsTheater\Regisseur;
+use BitsTheater\Director; /* @var $director Director */
+use BitsTheater\Regisseur; /* @var $theStageManager Regisseur */
+use BitsTheater\models\Auth as AuthDB; /* @var $dbAuth AuthDB */
 
 //CLI options should be defined by the special function `process_cli_options($aStageManger)`
 function process_cli_options($aStageManager)
@@ -11,13 +13,23 @@ function process_cli_options($aStageManager)
 
 //standard means of setting up the environment to access the website via CLI
 //  the processed CLI options returned by the process_cli_options() are returned as well.
-$theCliOptions = require_once('cli_bootstrap.php');
+$theCliOptions = require_once(__DIR__ . DIRECTORY_SEPARATOR . 'cli_bootstrap.php');
 
 //additional options we wish to parse besides the default -u -p and --host
 $_POST['force_model_upgrade'] = isset($theCliOptions['f']);
 
 //raiseCurtain on our actor/method to execute
-$director->raiseCurtain('Admin', 'apiWebsiteUpgrade');
+try {
+	if ( $director->isInstalled() ) {
+		$dbAuth = $director->getProp('Auth');
+	}
+	if ( !empty($dbAuth) && $dbAuth->isExists($dbAuth->tnAuthAccounts) ) {
+		$director->raiseCurtain('Admin', 'apiWebsiteUpgrade');
+	}
+}
+catch (\Exception $x) {
+	print($x->getMessage().PHP_EOL);
+}
 
 //end output with an EOL so CLI prompt will alway appear correctly on a fresh line
 print(PHP_EOL);
