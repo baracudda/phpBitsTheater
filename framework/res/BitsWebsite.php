781aaac61e9bb820afbb3e571e458ec335763ecd
ryanf <ryanf@blackmoonit.com>
2018-11-30 14:41:14 -0700

    pre-bump to [NEXT].

--- framework/res/BitsWebsite.php
+++ framework/res/BitsWebsite.php
@@ -22,9 +22,9 @@ use BitsTheater\res\Resources as BaseResources;
 class BitsWebsite extends BaseResources
 {
 	/** @var string The framework's sequence number. */
-	public $framework_version_seq = 12;
+	public $framework_version_seq = 13;
 	/** @var string The framework's displayed version number. */
-	public $framework_version = '4.2.2';
+	public $framework_version = '[NEXT]';
 	/**
 	 * Your website's build number.
 	 * This should only ever increase with each release you make.
