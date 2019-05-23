e8a6082f53401204cb0810b71ef394f7573de9b1
Ryan Fischbach <ryan.fischbach@istresearch.com>
2019-05-23 17:32:29 -0400

    email reset link venue added.

---
+++ framework/app/costumes/venue/TicketViaResetLink.php
@@ -0,0 +1,16 @@
+<?php
+namespace BitsTheater\costumes\venue;
+use BitsTheater\costumes\Wardrobe\TicketViaResetLink as BaseCostume;
+{//namespace begin
+
+/**
+ * Class used to help manage logging in via the password reset link.
+ * @since BitsTheater [NEXT]
+ */
+class TicketViaResetLink extends BaseCostume
+{
+	//nothing to override, yet
+	
+}//end class
+
+}//end namespace
