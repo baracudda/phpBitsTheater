140649a2b27d1d6cf44fc50724554506279d8067
Ryan Fischbach <ryan.fischbach@istresearch.com>
2020-09-16 10:04:34 -0400

    update framework_version_seq to 14.

--- framework/res/BitsWebsite.php
+++ framework/res/BitsWebsite.php
@@ -22,7 +22,7 @@ use BitsTheater\res\Resources as BaseResources;
 class BitsWebsite extends BaseResources
 {
 	/** @var string The framework's sequence number. */
-	public $framework_version_seq = 13;
+	public $framework_version_seq = 14;
 	/** @var string The framework's displayed version number. */
 	public $framework_version = '5.0.0';
 	/**
